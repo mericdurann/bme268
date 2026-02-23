9b4b34f9a7bc6d84016ebd924ee985ddd2641938
mericdurann <mericduran26@gmail.com>
2026-02-23 11:55:23 +0300

    first lecture

---
+++ hello.py
@@ -0,0 +1,21 @@
+x=12.4
+y=10
+z=y%3
+print(z) 
+z=y//3
+print(z)
+z=y**3
+print(z)
+print(int (x))
+print(float(x) + float(y))
+print (int(x) + y)
+
+x=12.4
+y=10
+z="123"
+print(int(z)+ x)
+
+print (float(z) + x)
+
+nam = input ('Who are you?')
+print('Welcome' , nam)
---
+++ hello2.py
@@ -0,0 +1,7 @@
+
+lecture = input ('How many lectures do you have in a week? ')
+month = (int(lecture) * 4)
+print('You have', month , 'lectures in a month' )
+
+"""calculates the lectures in a month"""
+
