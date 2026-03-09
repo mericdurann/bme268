1e90fe39968b5ecc7650b616ca40d7f1faf57cad
mericdurann <mericduran26@gmail.com>
2026-03-09 10:51:43 +0300

    exercise

--- exercise5.py
+++ exercise5.py
@@ -1,9 +1,12 @@
+#example1
+
 def first_function():
     print("Hello World")
     print("Example code")
 
 first_function()
 
+#example2
 
 def sum(x,y):
     print(x+y)
---
+++ exercise7.py
@@ -0,0 +1,32 @@
+
+#example1
+
+i=10
+
+while i>0:
+    print(i)
+    i = i-1
+
+print("test done!")
+
+#example2 
+
+while True:
+    x = input("Enter a something: ")
+    if x == "done":
+        break
+    else:
+        print("enter again")
+
+print ("done!")
+
+#example3
+
+while True:
+    line = input('> ')
+    if line[0] == '#' : 
+        continue
+    if line == 'done' :
+        break
+    print(line)
+print('Done!')
