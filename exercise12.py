94345f4458fa84b3020cfcd4eabea4092706239f
mericdurann <mericduran26@gmail.com>
2026-03-09 12:16:08 +0300

    exercise

---
+++ exercise12.py
@@ -0,0 +1,12 @@
+fruit = input("Enter a fruit: ")
+
+if fruit == "banana" or fruit == "lemon":
+    print("yellow")
+elif fruit == "apple" or fruit == "strawberry":
+    print("red")
+elif fruit == "grape" or fruit == "avocado" or fruit == "kiwi":
+    print("green")
+else:
+    print("I don't know that fruit")
+
+    
